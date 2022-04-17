8f01d44a9df092106280ef492ce456dc9f1ef0fa
Egor1515 <96645276+Egor1515@users.noreply.github.com>
2022-04-17 17:45:27 +1000

    Fix

--- src/test/java/SelenideTest.java
+++ src/test/java/SelenideTest.java
@@ -1,15 +1,6 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
 import java.time.Duration;
 
 import static com.codeborne.selenide.Selectors.withText;
@@ -17,33 +8,6 @@ import static com.codeborne.selenide.Selenide.*;
 
 
 public class SelenideTest {
-    private WebDriver driver;
-
-
-    @Test
-    @BeforeAll
-    static void setupClass() {
-        WebDriverManager.chromedriver().setup();
-    }
-
-    @BeforeEach
-    void setupTest() {
-
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
-        driver.get("http://localhost:7777/");
-    }
-
-
-    @AfterEach
-    void teardown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
 
     @Test
     public void shouldSendForm1() {
@@ -51,190 +15,15 @@ public class SelenideTest {
         Configuration.holdBrowserOpen = true;
         Configuration.browserSize = "1980x900";
         open("http://localhost:7777/");
-        $x("//input[@placeholder = 'Город']").val("Владивосток");
-        $(".icon-button__content").click();
-        $x("//div[@data-step ='1']").click();
-        $x("//td[@data-day='1651327200000']").click();
-        $x("//input[@name= 'name']").val("Петр Петров");
-        $x("//input[@name= 'phone']").val("+79148141312");
-        $(".checkbox__box").click();
-        $(withText("Забронировать")).click();
-        $(".notification__title").should(Condition.visible, Duration.ofSeconds(15));
-
-    }
-
-    @Test
-
-    public void shouldSendForm2() {
-        Configuration.holdBrowserOpen = true;
-        Configuration.browserSize = "1980x900";
-        open("http://localhost:7777/");
         $x("//input[@placeholder = 'Город']").val("Санкт-Петербург");
         $(".icon-button__content").click();
-        $x("//div[@data-step ='12']").click();
-        $x("//td[@data-day='1681048800000']").click();
-        $x("//input[@name= 'name']").val("Константин Иванов");
-        $x("//input[@name= 'phone']").val("+79148141312");
-        $(".checkbox__box").click();
-        $(withText("Забронировать")).click();
-        $(".notification__title").should(Condition.visible, Duration.ofSeconds(15));
-
-    }
-
-    @Test
-
-    public void shouldSendForm3() {
-        Configuration.holdBrowserOpen = true;
-        Configuration.browserSize = "1980x900";
-        open("http://localhost:7777/");
-        $x("//input[@placeholder = 'Город']").val("Йошкар-Ола");
-        $(".icon-button__content").click();
-        $x("//div[@data-step ='12']").click();
-        $x("//div[@data-step ='1']").click();
-        $x("//td[@data-day='1683295200000']").click();
-        $x("//input[@name= 'name']").val("Петров Петр");
-        $x("//input[@name= 'phone']").val("+79148141312");
-        $(".checkbox__box").click();
-        $(withText("Забронировать")).click();
-        $(".notification__title").should(Condition.visible, Duration.ofSeconds(15));
-
-    }
-
-    @Test
-
-    public void shouldSendForm4() {
-        Configuration.holdBrowserOpen = true;
-        Configuration.browserSize = "1980x900";
-        open("http://localhost:7777/");
-        $x("//input[@placeholder = 'Город']").val("Горно-Алтайск");
-        $(".icon-button__content").click();
-        $x("//div[@data-step ='12']").click();
-        $x("//div[@data-step ='-1']").click();
-        $x("//div[@data-step ='-1']").click();
-        $x("//td[@data-day='1675260000000']").click();
-        $x("//input[@name= 'name']").val("Петров Петр");
-        $x("//input[@name= 'phone']").val("+79148141312");
-        $(".checkbox__box").click();
-        $(withText("Забронировать")).click();
-        $(".notification__title").should(Condition.visible, Duration.ofSeconds(15));
-
-    }
-
-    @Test
-
-    public void shouldSendForm5() {
-        Configuration.holdBrowserOpen = true;
-        Configuration.browserSize = "1980x900";
-        open("http://localhost:7777/");
-        $x("//input[@placeholder = 'Город']").val("Сыктывкар");
-        $(".icon-button__content").click();
-        $x("//div[@data-step ='12']").click();
-        $x("//div[@data-step ='1']").click();
-        $x("//div[@data-step ='-12']").click();
-        $x("//td[@data-day='1651759200000']").click();
-        $x("//input[@name= 'name']").val("Петров Петр");
-        $x("//input[@name= 'phone']").val("+79148141312");
-        $(".checkbox__box").click();
-        $(withText("Забронировать")).click();
-        $(".notification__title").should(Condition.visible, Duration.ofSeconds(15));
-
-    }
-
-    @Test
-
-    public void shouldSendForm6() {
-        Configuration.holdBrowserOpen = true;
-        Configuration.browserSize = "1980x900";
-        open("http://localhost:7777/");
-        $x("//input[@placeholder = 'Город']").val("Салехард");
-        $x("//input[@placeholder ='Дата встречи']").sendKeys(Keys.BACK_SPACE);
-        $x("//input[@placeholder ='Дата встречи']").sendKeys(Keys.BACK_SPACE);
-        $x("//input[@placeholder ='Дата встречи']").sendKeys(Keys.BACK_SPACE);
-        $x("//input[@placeholder ='Дата встречи']").sendKeys(Keys.BACK_SPACE);
-        $x("//input[@placeholder ='Дата встречи']").sendKeys(Keys.BACK_SPACE);
-        $x("//input[@placeholder ='Дата встречи']").sendKeys(Keys.BACK_SPACE);
-        $x("//input[@placeholder ='Дата встречи']").sendKeys(Keys.BACK_SPACE);
-        $x("//input[@placeholder ='Дата встречи']").sendKeys(Keys.BACK_SPACE);
-        $x("//input[@placeholder ='Дата встречи']").val("11.12.2023");
-        $x("//input[@name= 'name']").val("Петров Петр");
-        $x("//input[@name= 'phone']").val("+79148141312");
-        $(".checkbox__box").click();
-        $(withText("Забронировать")).click();
-        $(".notification__title").should(Condition.visible, Duration.ofSeconds(15));
-
-    }
-
-    @Test
-
-    public void shouldSendForm7() {
-        Configuration.holdBrowserOpen = true;
-        Configuration.browserSize = "1980x900";
-        open("http://localhost:7777/");
-        $x("//input[@placeholder = 'Город']").val("Москва");
-        $x("//input[@placeholder ='Дата встречи']").doubleClick();
-        $x("//input[@placeholder ='Дата встречи']").sendKeys(Keys.BACK_SPACE);
-        $x("//input[@placeholder ='Дата встречи']").val("11.12.2023");
-        $x("//input[@name= 'name']").val("Петров Константин");
-        $x("//input[@name= 'phone']").val("+79148141312");
-        $(".checkbox__box").click();
-        $(withText("Забронировать")).click();
-        $(".notification__title").should(Condition.visible, Duration.ofSeconds(15));
-
-    }
-
-    @Test
-
-    public void shouldSendForm8() {
-        Configuration.holdBrowserOpen = true;
-        Configuration.browserSize = "1980x900";
-        open("http://localhost:7777/");
-        $x("//input[@placeholder = 'Город']").val("Владивосток");
-        $(".icon-button__content").click();
         $x("//div[@data-step ='1']").click();
         $x("//td[@data-day='1651327200000']").click();
-        $x("//input[@name= 'name']").val("Андрей Ю");
+        $x("//input[@name= 'name']").val("Петр Петров-Константинов");
         $x("//input[@name= 'phone']").val("+79148141312");
         $(".checkbox__box").click();
         $(withText("Забронировать")).click();
         $(".notification__title").should(Condition.visible, Duration.ofSeconds(15));
 
     }
-
-    @Test
-
-    public void shouldSendForm9() {
-        Configuration.holdBrowserOpen = true;
-        Configuration.browserSize = "1980x900";
-        open("http://localhost:7777/");
-        $x("//input[@placeholder = 'Город']").val("Владивосток");
-        $(".icon-button__content").click();
-        $x("//div[@data-step ='1']").click();
-        $x("//td[@data-day='1651327200000']").click();
-        $x("//input[@name= 'name']").val("Петров Петр");
-        $x("//input[@name= 'phone']").val("+79148141312");
-        $(".checkbox__box").click();
-        $(withText("Забронировать")).click();
-        $(".notification__title").should(Condition.visible, Duration.ofSeconds(15));
-
-    }
-
-    @Test
-
-    public void shouldSendForm10() {
-        Configuration.holdBrowserOpen = true;
-        Configuration.browserSize = "1980x900";
-        open("http://localhost:7777/");
-        $x("//input[@placeholder = 'Город']").val("Горно-Алтайск");
-        $(".icon-button__content").click();
-        $x("//div[@data-step ='12']").click();
-        $x("//div[@data-step ='-1']").doubleClick();
-        $x("//td[@data-day='1675260000000']").click();
-        $x("//input[@name= 'name']").val("Петров Петр");
-        $x("//input[@name= 'phone']").val("+79148141312");
-        $(".checkbox__box").click();
-        $(withText("Забронировать")).click();
-        $(".notification__title").should(Condition.visible, Duration.ofSeconds(15));
-
-    }
-
 }
